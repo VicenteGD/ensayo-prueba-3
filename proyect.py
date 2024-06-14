6e8b8a709546eee82be0d1856fea8761f954d74c
vicenteGD <vi.garridod@duocuc.cl>
2024-06-14 13:19:56 -0400

    menú lsito

---
+++ proyect.py
@@ -0,0 +1,18 @@
+import os, time
+
+while True:
+    print("menu trabajadores")
+    print("1. registrar trabajador")
+    print("2. listar todos los trabajadores")
+    print("3. imprimir planilla de sueldos ")
+    print("4. salir del programa")
+    opc = int(input("ingrese opción: "))
+    os.system("cls")
+    if opc ==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc == 3:
+        pass
+    elif opc==4:
+        pass
